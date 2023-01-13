77db77d83f1df23e941c275976e37bf4393a812f
IhorKarp <dogfrench777@gmail.com>
2023-01-14 00:19:10 +0200

    Added Github link j+ footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import SearchEngine from './SearchEngine';
+import github from "./pictures/github.png";
 
 function App() {
   return (
     <div className="App">
      <SearchEngine defaultKeyword="love" />
+     <footer className='text-white'>
+          <p className="text-center">
+            <a
+              href="https://github.com/IhorKarp/dictionary-web-app-react"
+              target="_blank"
+              className="open-source text-decoration-none text-dark"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="github-icon" width={30} />
+            </a>{" "}
+            coded by Ihor Karp
+          </p>
+        </footer>
     </div>
   );
 }
